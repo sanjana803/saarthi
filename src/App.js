21f926ed2838527c53537cc0b12e0bb9209bf2b6
sanjana803 <kumarisanjana.cer18@itbhu.ac.in>
2021-11-04 10:50:02 +0530

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import "./styles.css";
+import song from './sufi.mp3';
+import Waveform from "./Waveform";
+
+ const url = "https://www.mfiles.co.uk/mp3-downloads/gs-cd-track2.mp3";
+
+export default function App() {
+  const [selectedTrack, setSelectedTrack] = useState(song);
+
+  return (
+    <div className="App">
+     <h1> My Player</h1>
+      <Waveform url={selectedTrack} />     
+      <br />
+    </div>
+  );
+}
---
+++ src/Waveform.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useRef, useState } from "react";
+import './button.css'
+
+import WaveSurfer from "wavesurfer.js";
+
+const formWaveSurferOptions = ref => ({
+  container: ref,
+  waveColor: "#eee",
+  progressColor: "OrangeRed",
+  cursorColor: "OrangeRed",
+  barWidth: 3,
+  barRadius: 3,
+  responsive: true,
+  height: 150,
+  normalize: true,
+  partialRender: true
+});
+
+export default function Waveform({ url }) {
+  const waveformRef = useRef(null);
+  const wavesurfer = useRef(null);
+  const [playing, setPlay] = useState(false);
+  const [volume, setVolume] = useState(0.5);
+
+  useEffect(() => {
+    setPlay(false);
+
+    const options = formWaveSurferOptions(waveformRef.current);
+    wavesurfer.current = WaveSurfer.create(options);
+
+    wavesurfer.current.load(url);
+
+    wavesurfer.current.on("ready", function() {
+      if (wavesurfer.current) {
+        wavesurfer.current.setVolume(volume);
+        setVolume(volume);
+      }
+    });
+    return () => wavesurfer.current.destroy();
+  }, [url]);
+
+  const handlePlayPause = () => {
+    setPlay(!playing);
+    wavesurfer.current.playPause();
+  };
+
+  const onVolumeChange = e => {
+    const { target } = e;
+    const newVolume = +target.value;
+
+    if (newVolume) {
+      setVolume(newVolume);
+      wavesurfer.current.setVolume(newVolume || 1);
+    }
+  };
+
+  return (
+    <div>
+      <div id="waveform" ref={waveformRef} />
+      <div className="controls">
+        <div className='button'> 
+        <div onClick={handlePlayPause} className={!playing ? 'btn-play' : 'btn-stop'}></div></div>
+        <input
+          type="range"
+          id="volume"
+          name="volume"
+          min="0.01"
+          max="1"
+          step=".025"
+          onChange={onVolumeChange}
+          defaultValue={volume}
+        />
+        <label htmlFor="volume" className='vol'>Volume</label>
+      </div>
+    </div>
+  );
+}
